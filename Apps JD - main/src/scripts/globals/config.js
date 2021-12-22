c8d6997ac1cab91b2b584593bf523be00c312016
Syariful Musthofa <60634669+SyarifulMsth@users.noreply.github.com>
2021-12-22 12:14:02 +0700

    Delete Apps JD - main directory

--- Apps JD - main/src/scripts/globals/config.js
+++
@@ -1,8 +0,0 @@
-const CONFIG = {
-  KEY: 'AIzaSyDPKTf6zDaqv477Ovqb30eVT9EgTSGenkg',
-  BASE_URL: 'https://www.googleapis.com/books/v1/volumes',
-};
-
-export default CONFIG;
-
-// 'https://www.googleapis.com/books/v1/volumes?q='
