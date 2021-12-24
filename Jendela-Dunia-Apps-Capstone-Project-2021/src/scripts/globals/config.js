2bd16e475b3d44f71b6f9da8c5bdc75a8ac643e5
Dimas nurhamzah <95202608+DimasHamzah@users.noreply.github.com>
2021-12-24 23:58:45 +0700

    update api
    update finish api google books

--- Jendela-Dunia-Apps-Capstone-Project-2021/src/scripts/globals/config.js
+++ Jendela-Dunia-Apps-Capstone-Project-2021/src/scripts/globals/config.js
@@ -1,8 +1,9 @@
 const CONFIG = {
   KEY: 'AIzaSyDPKTf6zDaqv477Ovqb30eVT9EgTSGenkg',
   BASE_URL: 'https://www.googleapis.com/books/v1/volumes',
+  DATABASE_NAME: 'books-catalog-database',
+  DATABASE_VERSION: 1,
+  OBJECT_STORE_NAME: 'books',
 };
 
 export default CONFIG;
-
-// 'https://www.googleapis.com/books/v1/volumes?q='
