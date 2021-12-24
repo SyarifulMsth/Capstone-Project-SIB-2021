cfd02f072d4a6781db517ba0c4731e6e35bc1138
Syariful Musthofa <60634669+SyarifulMsth@users.noreply.github.com>
2021-12-24 23:54:35 +0700

    :new: menambahkan dan update file templates

--- Jendela-Dunia-Apps-Capstone-Project-2021/src/scripts/views/templates/proggres-tamplte.js
+++ Jendela-Dunia-Apps-Capstone-Project-2021/src/scripts/views/templates/proggres-tamplte.js
@@ -5,12 +5,50 @@ class ProggresTamplte {
     } = books;
     const containerWihlist = document.querySelector('#proggress-items');
     containerWihlist.innerHTML += `
-      <li>
+      <div class="proggress-main">
         <h4>${title}</h4>
         <p>${publisher}</p>
-        <p>${pageCount}</p>
-      </li>
+         <p>isi halaman : ${pageCount}</p>
+         <div id="selamat"></div>
+      </div>  
+      <div class="proggress-user">
+        <input type="text" id="buku-dibaca" placeholder="buku yang telah di baca">
+        <input type="text" id="jam-perhari" placeholder="berapa jam perhari">
+        <button id="btn-inputuser">klik proggres</button>
+        <p>sisa halaman <span id="sisa"></span></p>
+        <p>jika membaca <span id="jam"></span> jam perhari maka akan tuntas dalam <span id="minggu"></span> </p>
+      </div>
     `;
+  }
+
+  static sisaPage(books) {
+    const { pageCount } = books;
+    const inputBukuDibaca = document.getElementById('buku-dibaca');
+    const inputJamPerhari = document.getElementById('jam-perhari');
+    const sisaElment = document.getElementById('sisa');
+    const jam = document.getElementById('jam');
+    // eslint-disable-next-line no-unused-vars
+    const minggu = document.getElementById('minggu');
+
+    document.getElementById('btn-inputuser').addEventListener('click', (event) => {
+      event.preventDefault();
+
+      sisaElment.innerText = pageCount - inputBukuDibaca.value;
+      jam.innerText = inputJamPerhari.value;
+      minggu.innerText = sisaElment.innerText / inputJamPerhari.value;
+      this.finishProggres(sisaElment.innerText);
+    });
+  }
+
+  static finishProggres(sisaHalaman) {
+    const selamat = document.getElementById('selamat');
+    if (sisaHalaman <= 0) {
+      selamat.innerText = 'selamat anda telah menyelesaikan buku ini';
+    } else if (sisaHalaman <= 10) {
+      selamat.innerText = 'semangat lagi, sisa halaman tinggal sedikit';
+    } else {
+      selamat.innerText = 'ayo giat membaca';
+    }
   }
 }
 
